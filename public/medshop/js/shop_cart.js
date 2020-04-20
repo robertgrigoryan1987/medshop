365a2d986bca7c656781f3a0b8a653f4587d4aeb
GrigRob <youngman87@mail.ru>
2020-04-20 19:31:22 +0300

    medshop main6

--- public/medshop/js/shop_cart.js
+++ public/medshop/js/shop_cart.js
@@ -15,27 +15,25 @@ $( document ).ready(function() {
         let product_name = $(this).data('name');
         let product_price = $(this).data('price');
         let product_image = $(this).data('image');
-        console.log(product_image);
+        console.log(product_image,'product_image');
 
-
-        $.ajax({
-            type: 'POST',
-            url: '/shop_cart',
-            data:{product_id:product_id, product_name:product_name, product_price:product_price, product_image:product_image} ,
-            error: function(data){
-                var errors = data.responseJSON;
-               console.log(errors);
-            },
-            success: function(resp){
-                var count_product = $('.count-product').text();
-                console.log(count_product);
-                count_product++;
-                console.log(count_product);
-
-                $('.count-product').text(count_product);
-            }
-
-        });
+        //
+        // $.ajax({
+        //     type: 'POST',
+        //     url: '/shop_cart',
+        //     data:{product_id:product_id, product_name:product_name, product_price:product_price, product_image:product_image} ,
+        //     error: function(data){
+        //         var errors = data.responseJSON;
+        //        console.log(errors);
+        //     },
+        //     success: function(resp){
+        //         var count_product = $('.count-product').text();
+        //         count_product++;
+        //
+        //         $('.count-product').text(count_product);
+        //     }
+        //
+        // });
 
 
     })
