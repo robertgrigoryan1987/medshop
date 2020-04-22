abdb8e377c55ae3b9726115af6111c4f52e393dd
artyom09 <artyom1996a@gmail.com>
2020-04-22 23:39:43 +0400

    blog sigle  page

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -232,50 +232,6 @@ $langages = Language::all();
         </div>
     </section>
 
-    <section class="rev_slider_wrapper">
-        <div id="slider1" class="rev_slider" data-version="5.0">
-            <ul>
-                <li data-transition="rs-20">
-                    <img src="/medshop/images/slides/1.jpg" alt="" width="1920" height="450" data-bgposition="top center" data-bgfit="cover" data-bgrepeat="no-repeat" data-bgparallax="1">
-
-                    <div class="tp-caption  tp-resizeme" data-x="left" data-hoffset="0" data-y="top" data-voffset="220" data-transform_idle="o:1;" data-transform_in="x:[-175%];y:0px;z:0;rX:0;rY:0;rZ:0;sX:1;sY:1;skX:0;skY:0;opacity:0.01;s:3000;e:Power3.easeOut;" data-transform_out="s:1000;e:Power3.easeInOut;s:1000;e:Power3.easeInOut;" data-mask_in="x:[100%];y:0;s:inherit;e:inherit;" data-splitin="none" data-splitout="none" data-responsive_offset="on" data-start="1500">
-                        <div class="slide-content-box mar-lft">
-                            <h1>Пищевая <br> Добавка <span>Добавка</span></h1>
-                            <p>Пищевая Добавка <br>Пищевая Добавка.</p>
-
-                        </div>
-                    </div>
-
-                </li>
-                <li data-transition="fade">
-                    <img src="/medshop/images/slides/2.jpg" alt="" width="1920" height="450" data-bgposition="top center" data-bgfit="cover" data-bgrepeat="no-repeat" data-bgparallax="1">
-
-                    <div class="tp-caption  tp-resizeme" data-x="right" data-hoffset="0" data-y="top" data-voffset="220" data-transform_idle="o:1;" data-transform_in="x:[-175%];y:0px;z:0;rX:0;rY:0;rZ:0;sX:1;sY:1;skX:0;skY:0;opacity:0.01;s:3000;e:Power3.easeOut;" data-transform_out="s:1000;e:Power3.easeInOut;s:1000;e:Power3.easeInOut;" data-mask_in="x:[100%];y:0;s:inherit;e:inherit;" data-splitin="none" data-splitout="none" data-responsive_offset="on" data-start="1500">
-                        <div class="slide-content-box">
-                            <h1>Глянцевая <br> Железная <span>Таблетка.</span></h1>
-                            <p>Глянцевая Железная Таблетка <br>Глянцевая Железная Таблетка.</p>
-
-                        </div>
-                    </div>
-
-                </li>
-                <li data-transition="fade">
-                    <img src="/medshop/images/slides/3.jpg" alt="" width="1920" height="450" data-bgposition="top center" data-bgfit="cover" data-bgrepeat="no-repeat" data-bgparallax="1">
-
-                    <div class="tp-caption  tp-resizeme" data-x="left" data-hoffset="0" data-y="top" data-voffset="220" data-transform_idle="o:1;" data-transform_in="x:[-175%];y:0px;z:0;rX:0;rY:0;rZ:0;sX:1;sY:1;skX:0;skY:0;opacity:0.01;s:3000;e:Power3.easeOut;" data-transform_out="s:1000;e:Power3.easeInOut;s:1000;e:Power3.easeInOut;" data-mask_in="x:[100%];y:0;s:inherit;e:inherit;" data-splitin="none" data-splitout="none" data-responsive_offset="on" data-start="1500">
-                        <div class="slide-content-box mar-lft">
-                            <h1>Флакон <br> Для <span>Инъекций.</span></h1>
-                            <p>Флакон Для Инъекций <br>Флакон Для Инъекций.</p>
-
-                        </div>
-                    </div>
-
-                </li>
-            </ul>
-        </div>
-    </section>
-
-
             @yield('content')
 
         <footer class="footer-area">
