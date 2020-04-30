0a12850174850062884b602a02a2eeadf3f36990
GrigRob <youngman87@mail.ru>
2020-04-30 22:29:20 +0300

    about9

--- app/Http/Controllers/Voyager/VoyagerOrderProductsController.php
+++ app/Http/Controllers/Voyager/VoyagerOrderProductsController.php
@@ -19,7 +19,7 @@ use Illuminate\Support\Facades\Hash;
 class VoyagerOrderProductsController extends BaseVoyagerController
 {
     public function orderingProducts($id){
-        $orderig_products = OrderingProduct::all()->where('order_id', $id);
+        $orderig_products = OrderingProduct::all()->where('order_id', $id)->orWhere('session', '=', $id);
         return view('vendor.voyager.ordered.products')->with(compact('orderig_products'));
 
     }
