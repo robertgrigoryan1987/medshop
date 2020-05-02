f9092c3f5f5c266ab24f18f1e61cd6edc313f6f8
GrigRob <youngman87@mail.ru>
2020-05-02 23:11:15 +0300

    pas8

---
+++ app/Http/Controllers/Voyager/VoyagerCashProductsController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers\Voyager;
+use App\OrderingProduct;
+use App\Ordering;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+use ReflectionClass;
+use TCG\Voyager\Database\Schema\SchemaManager;
+use TCG\Voyager\Database\Schema\Table;
+use TCG\Voyager\Database\Types\Type;
+use TCG\Voyager\Events\BreadAdded;
+use TCG\Voyager\Events\BreadDeleted;
+use TCG\Voyager\Events\BreadUpdated;
+use TCG\Voyager\Facades\Voyager;
+use TCG\Voyager\Http\Controllers\VoyagerController as BaseVoyagerController;
+use Illuminate\Support\Facades\Hash;
+
+class VoyagerCashProductsController extends BaseVoyagerController
+{
+    public function cashProducts(){
+        $cash_products = Ordering::all()->where('cassh', 1)->get();
+        return view('vendor.voyager.ordered.cash_products')->with(compact('orderig_products'));
+
+    }
+
+    public function edit_password($id){
+        $company = Company::all()->where('id', $id)->first();
+        return view('vendor.voyager.company.edit_password')->with(compact('company', 'company'));
+    }
+
+    public function edit_password_post(Request $request, $id){
+        $company = Company::all()->where('id', $id)->first();
+        $request->validate([
+            'password' => 'required|confirmed|min:8',
+        ]);
+
+        $company->password = Hash::make($request->password);
+        if($company->save()){
+            return redirect('admin/companies')->with('status', 'Password succesfuly updated!');
+        }
+
+
+        return back();
+    }
+}
---
+++ resources/views/vendor/voyager/ordered/cash_products.blade.php
@@ -0,0 +1,239 @@
+@extends('voyager::master')
+@section('page_header')
+    <div class="container-fluid">
+        <h1 class="page-title">
+            Cash Orders
+        </h1>
+    </div>
+@stop
+
+@section('content')
+    <div class="page-content browse container-fluid">
+        @include('voyager::alerts')
+        <div class="row">
+            <div class="col-md-12">
+                <div class="panel panel-bordered">
+                    <div class="panel-body">
+                            <form method="get" class="form-search">
+                                <div id="search-input">
+                                    <div class="col-2">
+                                        <select id="search_key" name="key">
+                                            @foreach($searchNames as $key => $name)
+                                                <option value="{{ $key }}" @if($search->key == $key || (empty($search->key) && $key == $defaultSearchKey)) selected @endif>{{ $name }}</option>
+                                            @endforeach
+                                        </select>
+                                    </div>
+                                    <div class="col-2">
+                                        <select id="filter" name="filter">
+                                            <option value="contains" @if($search->filter == "contains") selected @endif>contains</option>
+                                            <option value="equals" @if($search->filter == "equals") selected @endif>=</option>
+                                        </select>
+                                    </div>
+                                    <div class="input-group col-md-12">
+                                        <input type="text" class="form-control" placeholder="{{ __('voyager::generic.search') }}" name="s" value="{{ $search->value }}">
+                                        <span class="input-group-btn">
+                                            <button class="btn btn-info btn-lg" type="submit">
+                                                <i class="voyager-search"></i>
+                                            </button>
+                                        </span>
+                                    </div>
+                                </div>
+
+                            </form>
+                        <div class="table-responsive">
+                            <table id="dataTable" class="table table-hover">
+                                <thead>
+                                <tr>
+                                    <th class="sorting" tabindex="0" aria-controls="dataTable" rowspan="1" colspan="1" aria-label="
+
+                                            : activate to sort column ascending" style="width: 20px;">
+                                        <input type="checkbox" class="select_all">
+                                    </th><th class="sorting" tabindex="0" aria-controls="dataTable" rowspan="1" colspan="1" aria-label="
+                                                                                        User Id
+                                                                                    : activate to sort column ascending" style="width: 58px;">
+                                        User Id
+                                    </th><th class="sorting" tabindex="0" aria-controls="dataTable" rowspan="1" colspan="1" aria-label="
+                                                                                        Customer City
+                                                                                    : activate to sort column ascending" style="width: 111px;">
+                                        Customer City
+                                    </th><th class="sorting" tabindex="0" aria-controls="dataTable" rowspan="1" colspan="1" aria-label="
+                                                                                        Customer Addres
+                                                                                    : activate to sort column ascending" style="width: 135px;">
+                                        Customer Addres
+                                    </th><th class="sorting" tabindex="0" aria-controls="dataTable" rowspan="1" colspan="1" aria-label="
+                                                                                        Customer Telephone
+                                                                                    : activate to sort column ascending" style="width: 162px;">
+                                        Customer Telephone
+                                    </th><th class="sorting" tabindex="0" aria-controls="dataTable" rowspan="1" colspan="1" aria-label="
+                                                                                        Order Status
+                                                                                    : activate to sort column ascending" style="width: 101px;">
+                                        Order Status
+                                    </th><th class="sorting" tabindex="0" aria-controls="dataTable" rowspan="1" colspan="1" aria-label="
+                                                                                        Curency
+                                                                                    : activate to sort column ascending" style="width: 67px;">
+                                        Curency
+                                    </th><th class="sorting" tabindex="0" aria-controls="dataTable" rowspan="1" colspan="1" aria-label="
+                                                                                        Amount
+                                                                                    : activate to sort column ascending" style="width: 66px;">
+                                        Amount
+                                    </th><th class="sorting" tabindex="0" aria-controls="dataTable" rowspan="1" colspan="1" aria-label="
+                                                                                        Created At
+                                                                                    : activate to sort column ascending" style="width: 138px;">
+                                        Created At
+                                    </th><th class="sorting" tabindex="0" aria-controls="dataTable" rowspan="1" colspan="1" aria-label="
+                                                                                        Email
+                                                                                    : activate to sort column ascending" style="width: 78px;">
+                                        Email
+                                    </th><th class="sorting" tabindex="0" aria-controls="dataTable" rowspan="1" colspan="1" aria-label="
+                                                                                        Order Id
+                                                                                    : activate to sort column ascending" style="width: 115px;">
+                                        Order Id
+                                    </th><th class="sorting" tabindex="0" aria-controls="dataTable" rowspan="1" colspan="1" aria-label="
+                                                                                        Cash
+                                                                                    : activate to sort column ascending" style="width: 42px;">
+                                        Cash
+                                    </th><th class="actions text-right sorting_disabled" rowspan="1" colspan="1" aria-label="Actions" style="width: 163px;">Actions</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                @foreach($cash_products as $product)
+                                    <tr>
+
+                                        <td>
+                                            <input type="checkbox" name="row_id" id="checkbox_1" value="1">
+                                        </td>
+                                        <td>
+                                            <div>{{$product->user_id}}</div>
+                                        </td>
+                                        <td>
+                                            <div>{{$product->customer_city}}</div>
+                                        </td>
+                                        <td>
+                                            <div>{{$product->customer_addres}}</div>
+                                        </td>
+                                        <td>
+                                            <div>{{$product->customer_telephone}}</div>
+                                        </td>
+                                        <td>
+                                            {{$product->order_status}}
+                                        </td>
+                                        <td>
+                                            <div>{{$product->curency}}</div>
+                                        </td>
+                                        <td>
+                                            <div>{{$product->amount}}</div>
+                                        </td>
+                                        <td>
+                                            {{$product->created_at}}
+                                        </td>
+                                        <td>
+                                            <div>{{$product->email}}</div>
+                                        </td>
+                                        <td>
+                                            <div>{{$product->order_id}}</div>
+                                        </td>
+                                        <td>
+                                            <div>{{$product->cash}}</div>
+                                        </td>
+
+
+                                        <td class="no-sort no-click bread-actions">
+                                            <a href="products_in_order/{{strlen($data->session)>4 ? $data->session : $data->id }}" class="btn btn-sm btn-warning pull-right edit">
+                                                <i class="voyager-edit"></i> <span class="hidden-xs hidden-sm">Products</span>
+                                            </a>
+
+                                            <a href="ordering-products/{{ $data->id }}" title="View" class="btn btn-sm btn-warning pull-right view">
+                                                <i class="voyager-eye"></i> <span class="hidden-xs hidden-sm">View</span>
+                                            </a>
+
+                                        </td>
+                                    </tr>
+                                @endforeach
+                                </tbody>
+                            </table>
+                        </div>
+                        @if ($isServerSide)
+                            <div class="pull-left">
+                                <div role="status" class="show-res" aria-live="polite">{{ trans_choice(
+                                    'voyager::generic.showing_entries', $dataTypeContent->total(), [
+                                        'from' => $dataTypeContent->firstItem(),
+                                        'to' => $dataTypeContent->lastItem(),
+                                        'all' => $dataTypeContent->total()
+                                    ]) }}</div>
+                            </div>
+                            <div class="pull-right">
+                                {{ $dataTypeContent->appends([
+                                    's' => $search->value,
+                                    'filter' => $search->filter,
+                                    'key' => $search->key,
+                                    'order_by' => $orderBy,
+                                    'sort_order' => $sortOrder,
+                                    'showSoftDeleted' => $showSoftDeleted,
+                                ])->links() }}
+                            </div>
+                        @endif
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    {{-- Single delete modal --}}
+    <div class="modal modal-danger fade" tabindex="-1" id="delete_modal" role="dialog">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="{{ __('voyager::generic.close') }}"><span aria-hidden="true">&times;</span></button>
+                    <h4 class="modal-title"><i class="voyager-trash"></i> {{ __('voyager::generic.delete_question') }} {{ strtolower($dataType->getTranslatedAttribute('display_name_singular')) }}?</h4>
+                </div>
+                <div class="modal-footer">
+                    <form action="#" id="delete_form" method="POST">
+                        {{ method_field('DELETE') }}
+                        {{ csrf_field() }}
+                        <input type="submit" class="btn btn-danger pull-right delete-confirm" value="{{ __('voyager::generic.delete_confirm') }}">
+                    </form>
+                    <button type="button" class="btn btn-default pull-right" data-dismiss="modal">{{ __('voyager::generic.cancel') }}</button>
+                </div>
+            </div><!-- /.modal-content -->
+        </div><!-- /.modal-dialog -->
+    </div><!-- /.modal -->
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="{{ voyager_asset('lib/css/responsive.dataTables.min.css') }}">
+@stop
+
+@section('javascript')
+    <!-- DataTables -->
+    @if(!$dataType->server_side && config('dashboard.data_tables.responsive'))
+        <script src="{{ voyager_asset('lib/js/dataTables.responsive.min.js') }}"></script>
+    @endif
+    <script>
+        $(document).ready(function () {
+
+            $('#search-input select').select2({
+                minimumResultsForSearch: Infinity
+            });
+
+            $('.select_all').on('click', function(e) {
+                $('input[name="row_id"]').prop('checked', $(this).prop('checked')).trigger('change');
+            });
+        });
+
+        var deleteFormAction;
+        $('td').on('click', '.delete', function (e) {
+            $('#delete_form')[0].action = '{{ route('voyager.'.$dataType->slug.'.destroy', '__id') }}'.replace('__id', $(this).data('id'));
+            $('#delete_modal').modal('show');
+        });
+
+        $('input[name="row_id"]').on('change', function () {
+            var ids = [];
+            $('input[name="row_id"]').each(function() {
+                if ($(this).is(':checked')) {
+                    ids.push($(this).val());
+                }
+            });
+            $('.selected_ids').val(ids);
+        });
+    </script>
+@stop
