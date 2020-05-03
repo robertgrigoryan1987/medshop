b368279abb8976deed37cad3e5a351591f7191b1
GrigRob <youngman87@mail.ru>
2020-05-03 11:05:23 +0300

    cash8

---
+++ app/AboutExperience.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: user
+ * Date: 03.05.2020
+ * Time: 11:04
+ */
+
+namespace App;
+namespace App;
+use Illuminate\Database\Eloquent\Model;
+use TCG\Voyager\Traits\Translatable;
+
+
+class AboutExperience extends Model
+{
+
+}
--- resources/views/vendor/voyager/ordered/cash_products.blade.php
+++ resources/views/vendor/voyager/ordered/cash_products.blade.php
@@ -14,32 +14,6 @@
             <div class="col-md-12">
                 <div class="panel panel-bordered">
                     <div class="panel-body">
-                            <form method="get" class="form-search">
-                                <div id="search-input">
-                                    <div class="col-2">
-                                        <select id="search_key" name="key">
-                                            @foreach($searchNames as $key => $name)
-                                                <option value="{{ $key }}" @if($search->key == $key || (empty($search->key) && $key == $defaultSearchKey)) selected @endif>{{ $name }}</option>
-                                            @endforeach
-                                        </select>
-                                    </div>
-                                    <div class="col-2">
-                                        <select id="filter" name="filter">
-                                            <option value="contains" @if($search->filter == "contains") selected @endif>contains</option>
-                                            <option value="equals" @if($search->filter == "equals") selected @endif>=</option>
-                                        </select>
-                                    </div>
-                                    <div class="input-group col-md-12">
-                                        <input type="text" class="form-control" placeholder="{{ __('voyager::generic.search') }}" name="s" value="{{ $search->value }}">
-                                        <span class="input-group-btn">
-                                            <button class="btn btn-info btn-lg" type="submit">
-                                                <i class="voyager-search"></i>
-                                            </button>
-                                        </span>
-                                    </div>
-                                </div>
-
-                            </form>
                         <div class="table-responsive">
                             <table id="dataTable" class="table table-hover">
                                 <thead>
