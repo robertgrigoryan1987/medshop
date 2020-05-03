9bd68b62d0efc1624304030a38329b279a16ad48
GrigRob <youngman87@mail.ru>
2020-05-03 15:06:59 +0300

    slide

---
+++ app/HomeSlider.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: user
+ * Date: 03.05.2020
+ * Time: 15:05
+ */
+
+namespace App;
+use Illuminate\Database\Eloquent\Model;
+use TCG\Voyager\Traits\Translatable;
+
+
+
+class HomeSlider extends Model
+{
+    public $timestamps = false;
+    use Translatable;
+    protected $translatable = ['title','text'];
+
+
+}
