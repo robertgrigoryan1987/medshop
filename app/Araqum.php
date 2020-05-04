60fb9820a7674ce9c855d7f2a642d23ec0c9125f
GrigRob <youngman87@mail.ru>
2020-05-04 20:29:33 +0300

    araqum

---
+++ app/Araqum.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: user
+ * Date: 03.05.2020
+ * Time: 16:17
+ */
+
+namespace App;
+use Illuminate\Database\Eloquent\Model;
+
+
+class Araqum extends Model
+{
+
+}
